ffc9aebefba4f9454f218abc72eaab14db320d26
Rentaro Mori <re_mori@yumemi.co.jp>
2024-04-14 06:50:30 +0900

    fix: main for check

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
+use core::num;
+use std::iter::Sum;
 use std::process;
 
 use resolve_algorithms::hello::hello;
 use resolve_algorithms::input::get_input;
+use resolve_algorithms::input_env::input_numbers;
 
 // Do solution
 fn main() {
-    let input = match get_input() {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("Error reading input: {}", e);
-            process::exit(1);
-        }
-    };
+    // let input = match get_input() {
+    //     Ok(s) => s,
+    //     Err(e) => {
+    //         eprintln!("Error reading input: {}", e);
+    //         process::exit(1);
+    //     }
+    // };
     
-    hello(input)
+    // hello(input)
+    let numbers = input_numbers();
+    let total: isize = numbers.iter().sum();
+    println!("{}", total);
+
 }
